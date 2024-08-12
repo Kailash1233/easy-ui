fcfd58ec748c4d4ac5f3a3c700722afb7187ffae
DarkInventor <ktmehta25@gmail.com>
2024-08-12 05:18:34 -0400

    add easy story

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -271,6 +271,16 @@ export default function TemplatePage() {
           <p className="text-base text-gray-600 dark:text-gray-400">Easy Grids Template for Grid Lovers</p>
         </MagicCard>
         <MagicCard
+          className="group cursor-pointer flex flex-col items-center justify-center overflow-hidden p-6 lg:p-10 sm:p-0 md:p-10 shadow-2xl"  
+          onClick={() => window.location.href='/ez-story'}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <img src="/ez-story.png" alt="EasyWaitlist" className="w-full h-auto" />
+          <h2 className="mt-4 text-lg font-bold text-gray-800 dark:text-gray-200">Easy Story</h2>
+          <p className="text-base text-gray-600 dark:text-gray-400">Easy Template for Telling a Small Story</p>
+        </MagicCard>
+        <MagicCard
           className="group cursor-pointer flex flex-col items-center justify-center overflow-hidden p-10 shadow-2xl"
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
