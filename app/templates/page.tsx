dd79478ddfc036ef318c9573ecf894b7cdd08df3
DarkInventor <ktmehta25@gmail.com>
2024-08-13 11:22:32 -0400

    add -easy newsletter template

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -281,6 +281,16 @@ export default function TemplatePage() {
           <p className="text-base text-gray-600 dark:text-gray-400">Easy Template for Telling a Small Story</p>
         </MagicCard>
         <MagicCard
+          className="group cursor-pointer flex flex-col items-center justify-center overflow-hidden p-6 lg:p-10 sm:p-0 md:p-10 shadow-2xl"  
+          onClick={() => window.location.href='/ez-newsletter'}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <img src="https://pub-0cd6f9d4131f4f79ac40219248ae64db.r2.dev/ez-newsletter.png" alt="EasyWaitlist" className="w-full h-auto" />
+          <h2 className="mt-4 text-lg font-bold text-gray-800 dark:text-gray-200">Easy Newsletter</h2>
+          <p className="text-base text-gray-600 dark:text-gray-400">Easy Template for Newsletter Landing</p>
+        </MagicCard>
+        <MagicCard
           className="group cursor-pointer flex flex-col items-center justify-center overflow-hidden p-10 shadow-2xl"
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
