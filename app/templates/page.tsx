e42f73c8c84de44198eb1ca29d81f44976dec5e1
DarkInventor <ktmehta25@gmail.com>
2024-08-22 13:52:40 -0400

    add easy-marketplace

--- app/templates/page.tsx
+++ app/templates/page.tsx
@@ -301,6 +301,16 @@ export default function TemplatePage() {
           <p className="text-base text-gray-600 dark:text-gray-400">Easy Landing Page template for Notes + Mini Notion Themed Editor</p>
         </MagicCard>
         <MagicCard
+          className="group cursor-pointer flex flex-col items-center justify-center overflow-hidden p-6 lg:p-10 sm:p-0 md:p-10 shadow-2xl"  
+          onClick={() => window.location.href='/ez-marketplace'}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <img src="https://pub-0cd6f9d4131f4f79ac40219248ae64db.r2.dev/ez-marketplace.png" alt="EasyWaitlist" className="w-full h-auto" />
+          <h2 className="mt-4 text-lg font-bold text-gray-800 dark:text-gray-200">Easy Marketplace</h2>
+          <p className="text-base text-gray-600 dark:text-gray-400">Easy Template for building E-commerce Websites</p>
+        </MagicCard>
+        <MagicCard
           className="group cursor-pointer flex flex-col items-center justify-center overflow-hidden p-10 shadow-2xl"
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
